e0d469baed7d0a26664762b7bd8ed67088a13026
Jakub Pawlikowski <pawlikoj@mcmaster.ca>
2023-01-29 21:46:42 -0500

    updated wifispeeds graphs

--- wifispeeds.php
+++ wifispeeds.php
@@ -13,18 +13,24 @@ echo "</div>";
 
 echo "<div class='flex-container'>";
 
-echo "<h2>Chess Tracker</h2>";
+echo "<h2>Wifi Speed Tests</h2>";
 
 echo "<ul>";
 echo "<li>Python tool written to track and visualize wifi speeds, downloads/uploads/pings</li>";
 echo "<li>Code and more information available at : https://github.com/JPawlikowski/pyWifiSpeedTest</li>";
 echo "</ul>";
 
-echo "<img src='downloads_202211282132.png'>";
+echo "<h4>Downloads</h4>";
 
-echo "<img src='uploads_202211282132.png'>";
+echo "<img src='downloads_202301292137.png'>";
 
-echo "<img src='pings_202211282132.png'>";
+echo "<h4>Uploads</h4>";
+
+echo "<img src='uploads_202301292137.png'>";
+
+echo "<h4>Pings</h4>";
+
+echo "<img src='pings_202301292137.png'>";
 
 echo "</div>";
 
